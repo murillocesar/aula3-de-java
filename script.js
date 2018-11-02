78a8a9719fccf52a664635cdb0a7a117f8b57f38
murillocesar <murillocbispo@gmail.com>
2018-11-01 21:50:47 -0300

    modificaçao de js, css e html

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Iniciou script');
+
+let nota = Number(prompt('Qual foi a sua nota?'));
+if (nota >= 6){
+    alert ('Parabens, voce foi aprovado!');
+}
+else if (nota >= 5.5 && entregouTudo) {
+    alert ('Você foi aprovado, mas agradeça o computador');
+}
+
+else{
+    alert ('Que pena, voce foi reprovado! :(');
+}
